127541708d47d4ee7239c2bfa3d69a15cf274d37
plan6062 <114268720+plan6062@users.noreply.github.com>
2025-07-05 14:55:23 +0900

    feat: tiny 서버 구현

--- tiny/cgi-bin/adder.c
+++ tiny/cgi-bin/adder.c
@@ -4,7 +4,38 @@
 /* $begin adder */
 #include "csapp.h"
 
-int main(void) {
+int main(void)
+{
+  char *buf, *p;
+  char arg1[MAXLINE], arg2[MAXLINE], content[MAXBUF];
+  int n1 = 0, n2 = 0;
+
+  /* Extract the two arguments */
+  if ((buf = getenv("QUERY_STRING")) != NULL)
+  {
+    p = strchr(buf, '&');
+    if (p)
+    {
+      *p = '\0';
+      strcpy(arg1, buf);
+      strcpy(arg2, p + 1);
+      n1 = atoi(arg1);
+      n2 = atoi(arg2);
+    }
+  }
+
+  /* Make the response body */
+  sprintf(content, "Welcome to add.com: THE Internet addition portal.\r\n<p>");
+  sprintf(content, "%sThe answer is: %d + %d = %d\r\n<p>",
+          content, n1, n2, n1 + n2);
+  sprintf(content, "%sThanks for visiting!\r\n", content);
+
+  /* Generate the HTTP response */
+  printf("Content-length: %d\r\n", (int)strlen(content));
+  printf("Content-type: text/html\r\n\r\n");
+  printf("%s", content);
+  fflush(stdout);
+
   exit(0);
 }
 /* $end adder */
